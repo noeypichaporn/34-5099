7a97cd39959eefe865141c3fa91731b7f987c671
noeypichaporn <36975@bpk.ac.th>
2024-12-23 14:33:45 +0700

    Add files via upload

---
+++ 334.py
@@ -0,0 +1,11 @@
+width = float(input())
+length = float(input())
+depth = float(input())
+
+volume = width * length * depth
+
+x = 15/ 60
+
+Total=volume*x
+
+print(f'ต้องใช้เวลาในการเติมน้ำ{Total : .2f}นาที')
---
+++ 34.py
@@ -0,0 +1,8 @@
+midterm = float(input())
+final =  float(input())
+
+total =  midterm + final
+average = total / 2
+
+print(f"Total: {total}")
+print(f"Average:{average}")
